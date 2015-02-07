17941ca178d6a6a01104281814ee58d1aef9459b
Chris McGinn <chris.s.mcginn@live.com>
2015-02-07 13:03:45 -0500

    Initial Project Creation

---
+++ Grid.Client/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grid.Client.Startup))]
+namespace Grid.Client
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
